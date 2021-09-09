faa03401c11ad653e8bde8665001ff5fcfa384a5
vicharmonic <vicharmonic@gmail.com>
2021-09-09 07:26:18 +0100

    b

--- webblocker.py
+++ webblocker.py
@@ -1,3 +1,13 @@
-print('Hello web')
-print('Hello web')
-print('Hello web')
+import time
+from datetime import datetime as dt
+
+hosts_temp = hosts
+# hosts_path = r"C:\Users\veecee\Desktop\New system\Codeo\Python\[FreeTutorials.Us] Udemy - the-python-mega-course\12 Application 3 Building a Website Blocker\120 Hosts"
+redirect = "127.0.0.1"
+website_list = ['www.facebook.com', 'facebook.com', 'pornographic videos']
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 7) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print('Working hours...')
+    else:
+        print('fun hours...')
+    time.sleep(5)
