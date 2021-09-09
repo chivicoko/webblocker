70eb15b4bf8b558b86e8ffb1d8a25d75eff58bfd
vicharmonic <vicharmonic@gmail.com>
2021-09-09 08:02:04 +0100

    d

--- webblocker.py
+++ webblocker.py
@@ -8,6 +8,14 @@ website_list = ['www.facebook.com', 'facebook.com', 'pornographic videos']
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 7) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print('Working hours...')
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            # print(content)
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write('\n' + redirect + ' ' + website)
     else:
         print('fun hours...')
     time.sleep(5)
