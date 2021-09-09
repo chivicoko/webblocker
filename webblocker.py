c608465636953c39a6850ac33a97ec7cb1aa8071
vicharmonic <vicharmonic@gmail.com>
2021-09-09 10:34:25 +0100

    e

--- webblocker.py
+++ webblocker.py
@@ -1,14 +1,14 @@
 import time
 from datetime import datetime as dt
 
-hosts_temp = 'hosts'
-# hosts_path = r"C:\Users\veecee\Desktop\New system\Codeo\Python\[FreeTutorials.Us] Udemy - the-python-mega-course\12 Application 3 Building a Website Blocker\120 Hosts"
+# hosts_temp = 'hosts'
+hosts_path = r"C:\Windows\System32"
 redirect = "127.0.0.1"
 website_list = ['www.facebook.com', 'facebook.com', 'pornographic videos']
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 7) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 11) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
         print('Working hours...')
-        with open(hosts_temp, 'r+') as file:
+        with open(hosts_path, 'r+') as file:
             content = file.read()
             # print(content)
             for website in website_list:
@@ -17,5 +17,12 @@ while True:
                 else:
                     file.write('\n' + redirect + ' ' + website)
     else:
+        with open(hosts_path, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print('fun hours...')
     time.sleep(5)
