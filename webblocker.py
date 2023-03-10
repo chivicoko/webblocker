0aa83415ceae5552e6d7c600e476f8e8e0ad29e7
chivicoko <vicharmonic@gmail.com>
2023-03-10 23:01:46 +0000

    update ...

--- webblocker.py
+++ webblocker.py
@@ -4,12 +4,12 @@ from datetime import datetime as dt
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 
 redirect = "127.0.0.1"
-website_list = ["www.facebook.com", "facebook.com", "www.youtube.com", "youtube.com"]
+website_list = ["www.facebook.com", "facebook.com"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Work hours...")
-        with open(hosts_temp, "r+") as file:
+        with open(hosts_path, "r+") as file:
             content = file.read()
             for website in website_list:
                 if website in content:
@@ -18,7 +18,7 @@ while True:
                     file.write(redirect + " " + website + "\n")
     else:
         print("Fun hours...")
-        with open(hosts_temp, "r+") as file:
+        with open(hosts_path, "r+") as file:
             content = file.readlines()
             file.seek(0)
             for line in content:
